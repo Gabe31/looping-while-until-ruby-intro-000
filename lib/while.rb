4a11223b059d1eed11cdd8b31dd553347a1dd737
Gabriel Aguilar <gabe.p.aguilar@gmail.com>
2020-08-31 08:09:56 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -6,11 +6,5 @@ def using_while
     puts "Wingardium Leviosa"
 end
 
-def using_until
-  levitation_force = 6
-  until levitation_force == 10
-    levitation_force += 1
-  puts "Wingardium Leviosa"
 
-end
 end
